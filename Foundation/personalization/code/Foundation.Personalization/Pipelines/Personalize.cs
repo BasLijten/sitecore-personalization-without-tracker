841275d76adb1de8105cd4828d6155e41856e989
Bas Lijten <bas.lijten@gmail.com>
2019-08-06 21:14:52 +0200

    initial commit

---
+++ Foundation/personalization/code/Foundation.Personalization/Pipelines/Personalize.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using BasLijten.Foundation.Personalization.Rules;
+using Sitecore;
+using Sitecore.Analytics.Pipelines.GetRenderingRules;
+using Sitecore.Analytics.Pipelines.RenderingRuleEvaluated;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Layouts;
+using Sitecore.Mvc.Analytics.Pipelines.Response.CustomizeRendering;
+using Sitecore.Mvc.Analytics.Presentation;
+using Sitecore.Rules;
+using Sitecore.Rules.ConditionalRenderings;
+
+namespace BasLijten.Foundation.Personalization.Pipelines
+{
+    public class FixPersonalize : Personalize
+    {
+        public override void Process(CustomizeRenderingArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            // the explicit check for "tracker.isactive" has been removed, as it prevents to evaluate conditions which should go off when there is no tracker
+
+            if (args.IsCustomized)
+            {
+                return;
+            }
+
+            this.Evaluate(args);
+        }
+
+        protected override void ApplyActions(CustomizeRenderingArgs args, ConditionalRenderingsRuleContext context)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            Assert.ArgumentNotNull(context, "context");
+            RenderingReference renderingReference = context.References.Find((RenderingReference r) => r.UniqueId == context.Reference.UniqueId);
+            if (renderingReference == null)
+            {
+                args.Renderer = new EmptyRenderer();
+                return;
+            }
+            this.ApplyChanges(args.Rendering, renderingReference);
+        }
+
+        protected override void Evaluate(CustomizeRenderingArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            Item item = args.PageContext.Item;
+            if (item == null)
+            {
+                return;
+            }
+            RenderingReference renderingReference = CustomizeRenderingProcessor.GetRenderingReference(args.Rendering, Context.Language, args.PageContext.Database);
+            GetRenderingRulesArgs expr_3E = new GetRenderingRulesArgs(item, renderingReference);
+            GetRenderingRulesPipeline.Run(expr_3E);
+            RuleList<ConditionalRenderingsRuleContext> ruleList = expr_3E.RuleList;
+            if (ruleList == null || ruleList.Count == 0)
+            {
+                return;
+            }
+
+            // after the ruleList has been retrieved AND ruleList > 0, instantiate a new personalization rulelist which has a fix to continue evaluation of conditions
+            // even when some exceptions occur.
+            PersonalizationRuleList<ConditionalRenderingsRuleContext> personalizationRuleList = new PersonalizationRuleList<ConditionalRenderingsRuleContext>();
+            foreach (var r in ruleList.Rules)
+            {
+                personalizationRuleList.Add(r);
+            }
+
+            ConditionalRenderingsRuleContext conditionalRenderingsRuleContext = new ConditionalRenderingsRuleContext(new List<RenderingReference>
+            {
+                renderingReference
+            }, renderingReference)
+            {
+                Item = item
+            };
+            conditionalRenderingsRuleContext.Parameters["mvc.rendering"] = args.Rendering;
+
+            // runRules for the personalizationRuleList
+            this.RunRules(personalizationRuleList, conditionalRenderingsRuleContext);
+            this.ApplyActions(args, conditionalRenderingsRuleContext);
+            args.IsCustomized = true;
+        }
+
+        /// <summary>
+        /// accept personalizationRuleList, evaluate conditions, 
+        /// </summary>
+        /// <param name="rules"></param>
+        /// <param name="context"></param>
+        protected virtual void RunRules(PersonalizationRuleList<ConditionalRenderingsRuleContext> rules, ConditionalRenderingsRuleContext context)
+        {
+            Assert.ArgumentNotNull(rules, "rules");
+            Assert.ArgumentNotNull(context, "context");
+            if (!RenderingRuleEvaluatedPipeline.IsEmpty())
+            {
+                rules.Evaluated += new RuleConditionEventHandler<ConditionalRenderingsRuleContext>(this.RulesEvaluatedHandler);
+            }
+
+            rules.RunFirstMatchingIgnoringTrackingExceptions(context);
+        }
+
+        private void RulesEvaluatedHandler(RuleList<ConditionalRenderingsRuleContext> ruleList, ConditionalRenderingsRuleContext ruleContext, Rule<ConditionalRenderingsRuleContext> rule)
+        {
+            RenderingRuleEvaluatedPipeline.Run(new RenderingRuleEvaluatedArgs(ruleList, ruleContext, rule));
+        }
+    }
+}
---
+++ Foundation/personalization/code/Foundation.Personalization/Rules/PersonalizationRuleList.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Sitecore.Configuration;
+using Sitecore.Diagnostics;
+using Sitecore.Rules;
+using Sitecore.Rules.Actions;
+using Sitecore.Rules.ConditionalRenderings;
+
+namespace BasLijten.Foundation.Personalization.Rules
+{
+    /// <summary>
+    /// should only be used for the conditionalrenderings
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class PersonalizationRuleList<T> : RuleList<T> where T : ConditionalRenderingsRuleContext
+    {        
+        public new event RuleConditionEventHandler<T> Evaluated;
+        public new event RuleConditionEventHandler<T> Evaluating;
+        public new event RuleActionEventHandler<T> Applied;
+
+        public void RunFirstMatchingIgnoringTrackingExceptions(T ruleContext)
+        {
+            Assert.ArgumentNotNull(ruleContext, "ruleContext");
+            int num;
+            this.RunIgnoreTrackingExceptions(ruleContext, true, out num);
+        }
+
+        // Run Forest Run!
+        protected virtual void RunIgnoreTrackingExceptions(T ruleContext, bool stopOnFirstMatching, out int executedRulesCount)
+        {
+            Assert.ArgumentNotNull(ruleContext, "ruleContext");
+            executedRulesCount = 0;
+            if (this.Count == 0)
+            {
+                return;
+            }
+            using (new LongRunningOperationWatcher(Settings.Profiling.RenderFieldThreshold, "Long running rule set: {0}", new string[]
+            {
+                this.Name ?? string.Empty
+            }))
+            {
+                foreach (Rule<T> current in this.Rules)
+                {
+                    if (current.Condition != null)
+                    {
+                        RuleStack ruleStack = new RuleStack();
+                        try
+                        {
+                            PersonalizationRuleList<T>.RaiseConditionEventHandler(this.Evaluating, this, ruleContext, current);
+                            current.Condition.Evaluate(ruleContext, ruleStack);
+                            PersonalizationRuleList<T>.RaiseConditionEventHandler(this.Evaluated, this, ruleContext, current);
+                        }
+                        // only skip when there is a tracker like operation
+                        catch (InvalidOperationException invalidOperation)
+                        {
+                            string arg = (current.Name != null) ? current.Name.ToString() : "Unknown";
+                            string arg2 = (current.Condition.UniqueId != null) ? current.Condition.UniqueId.ToString() : "Unknown";
+
+                            if (invalidOperation.Message.Contains("Tracker"))
+                            {
+                                Log.Warn(String.Format("Tracker.Current not available - rule with name: {0} skipped, condtion item ID: {1}", arg, arg2), invalidOperation, this);
+                            }
+                            else
+                            {
+                                Log.Error(string.Format("Evaluation of condition failed. Rule item ID: {0}, condition item ID: {1}", arg, arg2), invalidOperation, this);
+                                ruleContext.Abort();
+                            }
+                        }
+                        catch (Exception exception)
+                        {
+                            string arg = (current.Name != null) ? current.Name.ToString() : "Unknown";
+                            string arg2 = (current.Condition.UniqueId != null) ? current.Condition.UniqueId.ToString() : "Unknown";
+
+                            Log.Error(string.Format("Evaluation of condition failed. Rule item ID: {0}, condition item ID: {1}", arg, arg2), exception, this);
+                            ruleContext.Abort();
+                        }
+                        if (ruleContext.IsAborted)
+                        {
+                            break;
+                        }
+                        if (ruleStack.Count != 0)
+                        {
+                            bool flag = (bool)ruleStack.Pop();
+                            if (!flag || ruleContext.SkipRule)
+                            {
+                                ruleContext.SkipRule = false;
+                            }
+                            else
+                            {
+                                foreach (RuleAction<T> current2 in current.Actions)
+                                {
+                                    try
+                                    {
+                                        current2.Apply(ruleContext);
+                                        PersonalizationRuleList<T>.RaiseActionEventHandler(this.Applied, this, ruleContext, current2);
+                                    }
+                                    catch (Exception exception2)
+                                    {
+                                        string arg3 = (current.Name != null) ? current.Name : "Unknown";
+                                        string arg4 = (current2.UniqueId != null) ? current2.UniqueId.ToString() : "Unknown";
+                                        Log.Error(string.Format("Execution of action failed. Rule item ID: {0}, action item ID: {1}", arg3, arg4), exception2, this);
+                                        ruleContext.Abort();
+                                    }
+                                    if (ruleContext.IsAborted)
+                                    {
+                                        return;
+                                    }
+                                }
+                                executedRulesCount++;
+                                if (stopOnFirstMatching)
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void RaiseConditionEventHandler(RuleConditionEventHandler<T> eventHandler, RuleList<T> rules, T context, Rule<T> rule)
+        {
+            Assert.ArgumentNotNull(rules, "rules");
+            Assert.ArgumentNotNull(context, "context");
+            Assert.ArgumentNotNull(rule, "rule");
+            if (eventHandler == null)
+            {
+                return;
+            }
+            eventHandler(rules, context, rule);
+        }
+
+        private static void RaiseActionEventHandler(RuleActionEventHandler<T> eventHandler, RuleList<T> rules, T context, RuleAction<T> action)
+        {
+            Assert.ArgumentNotNull(rules, "rules");
+            Assert.ArgumentNotNull(context, "context");
+            Assert.ArgumentNotNull(action, "action");
+            if (eventHandler == null)
+            {
+                return;
+            }
+            eventHandler(rules, context, action);
+        }
+    }
+}
